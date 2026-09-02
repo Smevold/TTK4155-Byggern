1240477afc7fe212e175cb6e4ce8521739b41a6f
inamr03 <inaminrornes@gmail.com>
2026-09-02 17:54:57 +0200

    add receivver and tranciver to code

--- UART driver/main.c
+++ UART driver/main.c
@@ -1,22 +1,62 @@
 #include<avr/io.h>
 #include<util/delay.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 
 #define F_CPU 8000000UL		/* Define frequency here its 8MHz */
 #define UART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (UART_BAUDRATE * 16UL))) - 1)	
 
-void main (void)
-{
 
-    UART_Init (BAUD_PRESCALE);
-}
 
 void UART_Init( unsigned int ubrr)
 {
     UBRRH = (unsigned char)(ubrr>>8);
     UBRRL = (unsigned char) ubrr;
-    UCSRB = (1<<RXEN)|(1<<TXEN);
+    //Transmit enable
+    // receiver eable
+    UCSRB = (1 << RXEN)|(1 << TXEN);
+
+    // fram = 8 data, 2 stop
     
-    UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0);
+    UCSRC = (1 << URSEL) | (1 << UCSZO) | (3 << UCSZ0);
+}
+void UART_Transicver ( unsigned char data )
+{
+    // Data register empty
+    while ( !(UCSRA &(1 << UDRE)) )
+
+    // UDR to transfer to transmit data buffer reg
+    UDR = data; 
+}
+
+void UART_Receiver()
+{
+    while (( UCSRA &(1 << RXC)) == 0); // wait for data
+    return (UDR);
+}
+void send (char *str)
+{
+    unsigned char j = 0;
+
+    while (str[j]! = 0)
+    {
+        UART_Transicver(str[j]);
+        j++;
+    }
+}
+
+
+void main (void)
+{
+    char c;
+    
+    UART_Init (BAUD_PRESCALE);
+
+    UART_SendString("\n\t Echo Test ");
+    while (1)
+    {
+        c = UART_Receiver();
+        UART_Transicver(c); 
+    }
 }
