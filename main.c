a1c9d9c3072a5ace9f70da7b855cff20c4a0cbe6
inamr03 <inaminrornes@gmail.com>
2026-09-09 18:38:21 +0200

    masked JTAG

--- main.c
+++ main.c
@@ -13,16 +13,19 @@
 
 void pin_init(){
     MCUCR = (1 << SRE);
-    DDRA = 0b11111111;
-    DDRA |= (1 << DDA3) | (1 << DDA2) | (1 << DDA1) | (1 << DDA0);
-    DDRE |= (1 << DDE1);
-   
+    //DDRA = 0b11111111;
+    //DDRA |= (1 << DDA3) | (1 << DDA2) | (1 << DDA1) | (1 << DDA0);
+    //DDRE |= (1 << DDE1);
+    SFIOR |= (1 << XMM2);
+    SFIOR &= ~((1 << XMM1) | (1 << XMM0));
 }
 
+/*
 void pin_set(){
     PORTA = (1 << PA7) | (1 << PA6) | (1 << PA5) | (1 << PA4) | (1 << PA3) | (1 << PA2) | (1 << PA1) | (1 << PA0);
    // PORTE = (1 << PE1);
 }
+   */
 
 void main(){
     pin_init();
