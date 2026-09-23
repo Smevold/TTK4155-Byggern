680b7bf96197629953173a0fe7ea6e73c5526541
Inamr <inaminrornes@gmail.com>
2026-09-23 12:47:29 +0200

    add OLED driver files

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include "Drivers/UART_driver.h"
 #include "Drivers/IO_driver.h"
 #include "Drivers/SPI_driver.h"
+#include "Drivers/OLED_driver.h" 
 
 void pin_init(){ // Is probably only ext ram init
     MCUCR |= (1 << SRE);
