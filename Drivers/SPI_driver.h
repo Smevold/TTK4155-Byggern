656142c295d4a75d533a4d2ebc4a653bd6861187
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-23 14:39:56 +0200

    Finish SPI_driver functions

--- Drivers/SPI_driver.h
+++ Drivers/SPI_driver.h
@@ -2,8 +2,19 @@
 
 #include <stdint.h>
 
-void SPI_init();
+#define SS_IO_AVR 0
+#define SS_OLED 1
+#define SS_EMPTY 2
+
+void SPI_Init();
 
 void SPI_Transmit(char cData);
 
 char SPI_Receive();
+
+void SPI_Transmit_n_Bytes(char* data, uint8_t n);
+
+// Write "char* data[n];" to create input for this function
+void SPI_Receive_n_Bytes(char* data, uint8_t n);
+
+void SPI_SlaveSelect(uint8_t slave);
