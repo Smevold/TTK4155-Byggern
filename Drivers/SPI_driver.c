e65a4e78b2cddb0e53ada7bb471273131c326855
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-23 12:54:47 +0200

    Add SPI_Init, Transmit and Receive functions

--- Drivers/SPI_driver.c
+++ Drivers/SPI_driver.c
@@ -9,3 +9,26 @@
 
 #include "UART_driver.h"
 #include "IO_driver.h"
+
+void SPI_Init() {
+    // Set MOSI and SCK as output
+    DDR_SPI |= (1 << DD_MOSI) | (1 << DD_SCK);
+
+    // Enable SPI and Master, set clk rate to fck/16
+    SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+}
+
+void SPI_Transmit(char cData) {
+    // Start transmission
+    SPDR = cData;
+
+    // Wait for transmission completion
+    while (!(SPSR & (1 << SPIF))){;}
+}
+
+char SPI_Receive() {
+    // Wait for receive completion
+    while(!(SPSR & (1<<SPIF))){;}
+
+    return SPDR;
+}
--- Drivers/SPI_driver.h
+++ Drivers/SPI_driver.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <stdint.h>
+
+void SPI_init();
+
+void SPI_Transmit(char cData);
+
+char SPI_Receive();
